d693c769661370aaff66a9f0de5a917c48cd65e0
wellpereiradevs <132404088+wellpereiradevs@users.noreply.github.com>
2024-01-18 14:49:08 -0300

    Project Created

---
+++ src/main/java/com/wellpereiradevs/stock_china/StockChinaApplication.java
@@ -0,0 +1,13 @@
+package com.wellpereiradevs.stock_china;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockChinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockChinaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wellpereiradevs/stock_china/StockChinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellpereiradevs.stock_china;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockChinaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
