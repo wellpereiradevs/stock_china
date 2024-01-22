20477adf605c4e781c7d4a48929ac7715498bf3d
wellpereiradevs <132404088+wellpereiradevs@users.noreply.github.com>
2024-01-22 09:16:33 -0300

    GET AllProducts

--- src/main/java/com/wellpereiradevs/stock_china/controller/ProductController.java
+++ src/main/java/com/wellpereiradevs/stock_china/controller/ProductController.java
@@ -1,10 +1,23 @@
 package com.wellpereiradevs.stock_china.controller;
 
+import com.wellpereiradevs.stock_china.model.Products;
+import com.wellpereiradevs.stock_china.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Products> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
