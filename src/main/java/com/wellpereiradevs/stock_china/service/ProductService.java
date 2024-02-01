72574aa4620533fb1d89b485d93c3c120d22dc10
wellpereiradevs <132404088+wellpereiradevs@users.noreply.github.com>
2024-02-01 13:50:52 -0300

    Serviço de Produtos

---
+++ src/main/java/com/wellpereiradevs/stock_china/service/ProductService.java
@@ -0,0 +1,48 @@
+package com.wellpereiradevs.stock_china.service;
+
+import com.wellpereiradevs.stock_china.model.Products;
+import com.wellpereiradevs.stock_china.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    private ProductService (ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Products> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Products> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Products createProduct(Products products) {
+        return productRepository.save(products);
+    }
+
+    public Products updateProduct(Long id, Products products) {
+        if (!productRepository.existsById(id)) {
+            throw new EntityNotFoundException("Product not found with id: " + id);
+        }
+        products.setId(id);
+        return productRepository.save(products);
+    }
+
+    public void deleteProduct(Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new EntityNotFoundException("Product not found with id: " + id);
+        }
+        productRepository.deleteById(id);
+    }
+}
