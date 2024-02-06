2e5228ed59a04936a7893c73081176dceb4ec3f9
wellpereiradevs <132404088+wellpereiradevs@users.noreply.github.com>
2024-02-06 09:00:54 -0300

    Teste: getAllProducts criado

--- src/test/java/com/wellpereiradevs/stock_china/StockChinaApplicationTests.java
+++ src/test/java/com/wellpereiradevs/stock_china/StockChinaApplicationTests.java
@@ -8,13 +8,18 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest
 class StockChinaApplicationTests {
@@ -34,13 +39,30 @@ class StockChinaApplicationTests {
 
     @BeforeEach
     void setUp() {
-        Products product = new Products();
-        product.setId(1L);
-        product.setName("Product test");
-        product.setPrice_purchase(120.59);
-        product.setPrice_sale(369.99);
+        Products product1 = new Products();
+        product1.setId(1L);
+        product1.setName("Product test");
+        product1.setPrice_purchase(120.59);
+        product1.setPrice_sale(369.99);
 
-        when(productRepository.findAll()).thenReturn(List.of(product));
-        when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        Products product2 = new Products();
+        product2.setId(1L);
+        product2.setName("Product 2");
+        product2.setPrice_purchase(189.69);
+        product2.setPrice_sale(299.99);
+
+        List<Products> productsList = Arrays.asList(product1,product2);
+        when(productRepository.findAll()).thenReturn(productsList);
+    }
+
+    @Test
+    void getAllProducts() throws Exception{
+        List<Products> productsList = new ArrayList<>();
+        when(productRepository.findAll()).thenReturn(productsList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/products"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("R$[0].name").value(productsList.get(0).getName()))
+                .andExpect((ResultMatcher) jsonPath("R$[0].price").value(120.59));
     }
 }
