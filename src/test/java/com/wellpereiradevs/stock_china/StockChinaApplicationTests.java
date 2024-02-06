8223e47a0d02e31a199eb7a63b46f5b1b0a12a33
wellpereiradevs <132404088+wellpereiradevs@users.noreply.github.com>
2024-02-06 09:01:55 -0300

    Corrigindo a anotação do Mock

--- src/test/java/com/wellpereiradevs/stock_china/StockChinaApplicationTests.java
+++ src/test/java/com/wellpereiradevs/stock_china/StockChinaApplicationTests.java
@@ -12,6 +12,7 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -22,6 +23,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class StockChinaApplicationTests {
 
     @Test
